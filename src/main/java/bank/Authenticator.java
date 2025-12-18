8bdadc545437d13fbc96210cd4e8ec17b7fd2d9f
BEAZz23 <beatriceroccia98@gmail.com>
2025-12-18 10:01:40 +0000

    autenticazione

---
+++ src/main/java/bank/Authenticator.java
@@ -0,0 +1,22 @@
+package bank;
+
+import javax.security.auth.login.LoginException;
+
+public class Authenticator {
+
+  public static Customer login(String username, String password)theows LoginException{
+Customer customer=DataSource.getCustomer(username);
+if(customer==null){throw new LoginException("Username non trovate");
+
+}
+if(password.equals(customer.getPassword())){
+  customer.setAuthenticated(true);
+  return customer;
+}
+else throw new LoginException("Password errata");
+}
+
+  public static void logout(Customer customer){
+    customer.setAuthenticated(false);
+  }
+}
